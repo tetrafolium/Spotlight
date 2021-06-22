2262a0bb2ce328247b1db0bd7165dcbc9686f023
Inspecode <inspecode-bot@rocro.com>
2021-06-22 13:17:54 +0000

    Fix Artistic Style errors

--- spotlight/src/main/java/com/takusemba/spotlight/AbstractAnimatorListener.java
+++ spotlight/src/main/java/com/takusemba/spotlight/AbstractAnimatorListener.java
@@ -7,19 +7,19 @@ import android.animation.Animator;
  */
 public class AbstractAnimatorListener implements Animator.AnimatorListener {
 
-  @Override public void onAnimationStart(Animator animation) {
+    @Override public void onAnimationStart(Animator animation) {
 
-  }
+    }
 
-  @Override public void onAnimationEnd(Animator animation) {
+    @Override public void onAnimationEnd(Animator animation) {
 
-  }
+    }
 
-  @Override public void onAnimationCancel(Animator animation) {
+    @Override public void onAnimationCancel(Animator animation) {
 
-  }
+    }
 
-  @Override public void onAnimationRepeat(Animator animation) {
+    @Override public void onAnimationRepeat(Animator animation) {
 
-  }
+    }
 }
