e6cba2e2b2d853bfedd821d0f6c6922b53a22a71
Inspecode <inspecode-bot@rocro.com>
2021-06-24 15:23:54 +0000

    Fix Uncrustify errors

--- spotlight/src/main/java/com/takusemba/spotlight/AbstractAnimatorListener.java
+++ spotlight/src/main/java/com/takusemba/spotlight/AbstractAnimatorListener.java
@@ -7,15 +7,19 @@ import android.animation.Animator;
  */
 public class AbstractAnimatorListener implements Animator.AnimatorListener {
 
-  @Override
-  public void onAnimationStart(final Animator animation) {}
+@Override
+public void onAnimationStart(final Animator animation) {
+}
 
-  @Override
-  public void onAnimationEnd(final Animator animation) {}
+@Override
+public void onAnimationEnd(final Animator animation) {
+}
 
-  @Override
-  public void onAnimationCancel(final Animator animation) {}
+@Override
+public void onAnimationCancel(final Animator animation) {
+}
 
-  @Override
-  public void onAnimationRepeat(final Animator animation) {}
+@Override
+public void onAnimationRepeat(final Animator animation) {
+}
 }
